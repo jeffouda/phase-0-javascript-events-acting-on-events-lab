f4e74bb5f0c58065ffb7947d53435d81f31cc922
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-25 00:21:41 +0300

    test complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+// function to move left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// function to move right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    // game width is 400px, dodger is 40px
+    dodger.style.left = `${left + 1}px`;
+  }
+}
